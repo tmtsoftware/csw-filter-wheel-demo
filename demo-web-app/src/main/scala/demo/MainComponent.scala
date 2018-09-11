c5ff1d717584f1632b2df42c00230fa088f69d53
abrighton <78px3WT5s>
2018-09-11 14:46:43 +0200

    Fixed subscribe to events

--- demo-web-app/src/main/scala/demo/MainComponent.scala
+++ demo-web-app/src/main/scala/demo/MainComponent.scala
@@ -2,10 +2,13 @@ package demo
 
 import com.github.ahnfelt.react4s._
 import csw.messages.commands.{CommandName, Setup}
+import csw.messages.events.{Event, EventName, SystemEvent}
 import csw.messages.params.generics.{Key, KeyType}
 import csw.messages.params.models.{ObsId, Prefix}
 import org.scalajs.dom.EventSource
+import play.api.libs.json.Json
 import tmt.WebClients
+import tmt.assembly.codecs.AssemblyJsonSupport
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
@@ -14,10 +17,12 @@ object MainComponent {
   // XXX TODO: Get these values dynamically from the HCDs?
   val filters                = List("None", "g_G0301", "r_G0303", "i_G0302", "z_G0304", "Z_G0322", "Y_G0323", "u_G0308")
   val filterKey: Key[String] = KeyType.StringKey.make("filter")
+  val filterEventName        = EventName("filterEvent")
 
   val dispersers =
     List("Mirror", "B1200_G5301", "R831_G5302", "B600_G5303", "B600_G5307", "R600_G5304", "R400_G5305", "R150_G5306")
   val disperserKey: Key[String] = KeyType.StringKey.make("disperser")
+  val disperserEventName        = EventName("disperserEvent")
 
   val assemblyName = "DemoAssembly"
 
@@ -30,11 +35,8 @@ object MainComponent {
   private val obsId = ObsId("2023-Q22-4-33")
 }
 
-case class MainComponent() extends Component[NoEmit] {
+case class MainComponent() extends Component[NoEmit] with AssemblyJsonSupport {
   import MainComponent._
-
-//  // XXX temp: Timer handle, used until event service API for Scala.js is ready
-//  var interval: js.UndefOr[js.timers.SetIntervalHandle] = js.undefined
 
   subscribeToEvents()
 
@@ -59,9 +61,6 @@ case class MainComponent() extends Component[NoEmit] {
       Setup(demoPrefix, demoCmd, Some(obsId)).add(disperserKey.set(value))
     }
 
-//    // XXX temp timer
-//    interval = js.timers.setInterval(1000)(tick.runNow())
-
     assemblyClient.submit(setup).onComplete {
       case Success(response) => println(s"\nResponse $response")
       case Failure(ex) =>
@@ -74,7 +73,22 @@ case class MainComponent() extends Component[NoEmit] {
   private def subscribeToEvents(): Unit = {
     val client = new EventSource("http://localhost:9090/events/subscribe/subsystem/test")
     client.onmessage = { x =>
-      println(s"Received event: ${x.data}")
+      val data = x.data.toString
+      if (data.nonEmpty) {
+        Json.parse(data).as[Event] match {
+          case event: SystemEvent =>
+            println(s"Received event: $event")
+            if (event.eventName == filterEventName) {
+              val filter = event.get(filterKey).head.head
+              println(s"Selected filter from event: $filter")
+            } else if (event.eventName == disperserEventName) {
+              val disperser = event.get(disperserKey).head.head
+              println(s"Selected disperser from event: $disperser")
+            }
+          case event =>
+            println(s"Received unexpected event: $event")
+        }
+      }
     }
   }
 }
